90593a0a8f290e539da180fab8b424954359efb9
kosun <temparamental@hotmail.com>
2018-01-31 15:47:12 +0900

    JetBrains社のRiderでWPFアプリを作ってみた

---
+++ RiderWpfApp001/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RiderWpfApp001
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ RiderWpfApp001/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+
+namespace RiderWpfApp001.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _Title = "ほげもげ";
+
+        public string Title
+        {
+            get => _Title;
+            set => SetProperty(ref _Title, value);
+        }
+        
+        private string _Message = "ほげもげ";
+
+        public string Message
+        {
+            get => _Message;
+            set => SetProperty(ref _Message, value);
+        }
+    }
+}
